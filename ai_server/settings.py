1b23919dd2705d7cdcf72c36712a8c5bd8ce65f5
bushitan <373514952@qq.com>
2019-01-10 11:35:09 +0800

    1.2.3

--- ai_server/settings.py
+++ ai_server/settings.py
@@ -110,5 +110,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media") #pythonanywhere use
