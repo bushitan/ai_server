1832a5926dfbf77000abe054e3b69c54dabe7f21
bushitan <373514952@qq.com>
2019-01-10 10:37:39 +0800

    1.2.2

--- ai_server/settings.py
+++ ai_server/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'ai_server.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ai_server',
+        'USER': 'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
