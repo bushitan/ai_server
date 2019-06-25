0319fa08ae2e35fb3fc452ed01f97761fd6d20a4
“bushitan” <373514952@qq.com>
2019-06-26 01:29:35 +0800

    1.2.13

--- lib/qr_pil.py
+++ lib/qr_pil.py
@@ -8,25 +8,35 @@ class QRPrint():
 		pass
 
 	def create_page(self):
-		newIm= Image.new('RGB', (100, 100), 'red')
+		newIm= Image.new('RGBA', (100, 100), 'red')
 		newIm.save(self.base_path  + r'1.png')
 
-	def copy(self,bg_path ,qr_path):
+	def copy(self,bg_path ,qr_path,x,y):
 		im_bg = Image.open(bg_path)
-		im = Image.open(qr_path)
+		qr = Image.open(qr_path)
 		# cropedIm = im.crop((100,100,200,200))
-		im_bg.paste(im, (0, 0))
-		im_bg.show()
+		im_bg.paste(qr, (x, y), mask=qr)
+		# im_bg.show()
 		im_bg.save(self.base_path + r'paste.jpg')
 	def start(self):
-		bg = self.base_path  + r'mp.jpg'
-		qr = self.base_path  + r'wm.jpg'
-		self.copy(bg,qr)
+		bg = self.base_path  + r'bg.jpg'
+		qr = self.base_path  + r'wm_2UdX86fZ_200.png'
+		self.copy(bg,qr,68,601)
 
 if __name__ == "__main__":
 	q = QRPrint()
 	q.create_page()
 	q.start()
+
+
+# 2468  617
+# 3496  874
+
+# 620 877
+
+# 二维码位置  x=68 , y=601   w=200, h=200
+
+
 
 #
 #
